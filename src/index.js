b0b0a06d97c40ef9a82a8d113588dd30e26c6d05
Octavio Araiza <60367571+8ctavio@users.noreply.github.com>
2025-10-14 18:33:25 -0600

    feat: add `includeTypes` option to retrieve type export names

--- src/index.js
+++ src/index.js
@@ -7,73 +7,129 @@ import { ResolverFactory } from 'oxc-resolver'
  * @import { ValueSpan } from 'oxc-parser'
  */
 
-/** @type { ResolverFactory } */
-let resolver
-
-/** @type { () => ResolverFactory } */
-function useResolver() {
-	return resolver ??= new ResolverFactory({
-		conditionNames: ['import'],
-		extensions: [] // enable extension enforcement
-	})
+/** @type { ResolverFactory | undefined } */
+let resolverTypes
+/** @type { ResolverFactory | undefined } */
+let resolverImport
+const resolver = {
+	get types() {
+		if (!resolverTypes) {
+			const options = {
+				conditionNames: ["types", "import"],
+				extensions: [] // enable extension enforcement
+			}
+			resolverTypes = resolverImport
+				? resolverImport.cloneWithOptions(options)
+				: new ResolverFactory(options)
+		}
+		return resolverTypes
+	},
+	get values() {
+		if (!resolverImport) {
+			const options = {
+				conditionNames: ["import"],
+				extensions: [] // enable extension enforcement
+			}
+			resolverImport = resolverTypes
+				? resolverTypes.cloneWithOptions(options)
+				: new ResolverFactory(options)
+		}
+		return resolverImport
+	}
 }
 
 /**
- * @template { Set<string> | undefined } [ExportNames = Set<string>]
- * @template { boolean } [AsSet = boolean]
+ * @template { boolean | 'only' | 'separate' | undefined } [IncludeTypes = boolean | 'only' | 'separate']
+ * @template { Set<string> | readonly [Set<string>, Set<string>] | undefined } [ExportNames = Set<string> | readonly [Set<string>, Set<string>]]
+ * @template { boolean | undefined } [AsSet = boolean]
  * @typedef { object } ResolveModuleExportNamesOptions
- * @property { ExportNames } [exportNames] `Set` in which to add export names as they are found.
- * @property { AsSet } [asSet] If set to `true` a `Set` is returned; an `Array` is returned otherwise. Defaults to `false` if `exportNames` is not provided, and to `true` otherwise.
+ * @property {(
+ *     ExportNames & NoInfer<IncludeTypes extends 'separate' ? readonly [Set<string>, Set<string>] : Set<string>>
+ * )} [exportNames] 
+ *     `Set` in which to add export names as they are found.
+ * @property { AsSet } [asSet]
+ *     If set to `true` a `Set` is returned; an `Array` is returned otherwise.
+ *     Defaults to `false` if `exportNames` is not provided, and to `true` otherwise.
+ * @property { IncludeTypes } [includeTypes]
+ *     Whether to retrieve type export names.
+ * 
+ *     - If set to `only`, only type exports are retrieved.
+ *     - If set to `separate`, value and type export names are separately collected
+ *       into different arrays/sets. An array that stores the value and type export
+ *       name arrays/sets in that order is returned.
+ * 
+ *     Defaults to `false`.
  */
 
 /**
  * Returns the names of a module's exported values
  * 
- * @template { Set<string> | undefined } [ExportNames = undefined]
- * @template { boolean  } [AsSet = ExportNames extends Set<string> ? true : false]
+ * @template { ResolveModuleExportNamesOptions["includeTypes"] } [IncludeTypes = undefined]
+ * @template { ResolveModuleExportNamesOptions["exportNames"] } [ExportNames = undefined]
+ * @template { ResolveModuleExportNamesOptions["asSet"] } [AsSet = (
+ *     ExportNames extends (Set<string> | readonly [Set<string>, Set<string>]) ? true : false)
+ * ]
  * @overload
  * @param { string } specifier Module specifier.
  * @param { string } directory Reference directory path from which the module `specifier` is resolved.
- * @param { ResolveModuleExportNamesOptions<ExportNames, AsSet> } [options] Options object
+ * @param { ResolveModuleExportNamesOptions<IncludeTypes, ExportNames, AsSet> } [options] Options object
  * @returns {(
- * 		AsSet extends true ? Set<string> : string[]
+ *     AsSet extends true
+ *         ? IncludeTypes extends 'separate'
+ *             ? ExportNames extends readonly [Set<string>, Set<string>]
+ *                 ? ExportNames
+ *                 : [Set<string>, Set<string>]
+ *             : Set<string>
+ *         : IncludeTypes extends 'separate'
+ *             ? [string[], string[]]
+ *             : string[]
  * )} Export names
- */
-
+*/
 /**
  * Returns the names of a module's exported values
  * 
- * @template { Set<string> | undefined } [ExportNames = undefined]
- * @template { boolean  } [AsSet = ExportNames extends Set<string> ? true : false]
+ * @template { ResolveModuleExportNamesOptions["includeTypes"] } [IncludeTypes = undefined]
+ * @template { ResolveModuleExportNamesOptions["exportNames"] } [ExportNames = undefined]
+ * @template { ResolveModuleExportNamesOptions["asSet"] } [AsSet = (
+ *     ExportNames extends (Set<string> | readonly [Set<string>, Set<string>]) ? true : false)
+ * ]
  * @overload
  * @param { string } specifier Module specifier (absolute path to module file).
- * @param { ResolveModuleExportNamesOptions<ExportNames, AsSet> } [options] Options object
+ * @param { ResolveModuleExportNamesOptions<IncludeTypes, ExportNames, AsSet> } [options] Options object
  * @returns {(
- * 		AsSet extends true ? Set<string> : string[]
+ *     AsSet extends true
+ *         ? IncludeTypes extends 'separate'
+ *             ? ExportNames extends readonly [Set<string>, Set<string>]
+ *                 ? ExportNames
+ *                 : [Set<string>, Set<string>]
+ *             : Set<string>
+ *         : IncludeTypes extends 'separate'
+ *             ? [string[], string[]]
+ *             : string[]
  * )} Export names
  */
-
 /**
  * @param { string } specifier
  * @param {(
  *   | [directory: string, options?: ResolveModuleExportNamesOptions]
  *   | [options?: ResolveModuleExportNamesOptions]
  * )} args
- * @returns { string[] | Set<string> }
+ * @returns { string[] | Set<string> | readonly [string[], string[]] | readonly [Set<string>, Set<string>] }
  */
 export function resolveModuleExportNames(specifier, ...args) {
-	let modulePath = specifier
 	/** @type { ResolveModuleExportNamesOptions } */
 	let options
+	let modulePath = specifier
 	if (typeof args[0] === 'string') {
-		const [directory] = args
-		const resolve = useResolver()
+		/** @type { string } */
+		let directory
+		[directory, options = {}] = args
+		const resolve = resolver[options.includeTypes ? 'types' : 'values']
 		const resolveResult = resolve.sync(directory, specifier)
 		if (resolveResult.error) {
 			throw new Error(resolveResult.error)
 		}
 		modulePath = /** @type {string} */(resolveResult.path)
-		options = args[1] ?? {}
 	} else {
 		if (!isAbsolute(specifier)) {
 			throw new Error(String.prototype.concat.call(
@@ -81,7 +137,7 @@ export function resolveModuleExportNames(specifier, ...args) {
 				`received specifier "${specifier}"`
 			))
 		}
-		options = args[0] ?? {}
+		[options = {}] = args
 	}
 	
 	const parseResult = parseSync(
@@ -104,25 +160,65 @@ export function resolveModuleExportNames(specifier, ...args) {
 		}
 	}
 
+	const { includeTypes: typesConfig } = options
+	
+	const includeAll = typesConfig === true
+	const includeTypes = Boolean(typesConfig)
+	const includeTypesOnly = typesConfig === 'only'
+	const includeValuesOnly = !includeTypes
+	const separate = typesConfig === 'separate'
+	
+	const setProvided = 'exportNames' in options
+	
 	const {
-		exportNames = new Set(),
-		asSet = Boolean(options.exportNames)
+		exportNames = /** @type {Set<string> | readonly [Set<string>, Set<string>]} */
+			(separate ? [new Set(), new Set()] : new Set()),
+		asSet = setProvided,
 	} = options
+
+	if (setProvided && separate && !Array.isArray(exportNames)) {
+		throw new TypeError(String.prototype.concat.call(
+			"exportNames must be a two-Set Array when includeTypes is set to 'separate'; ",
+			`received ${exportNames}`
+		))
+	}
 
 	for (const staticExport of parseResult.module.staticExports) {
 		for (const entry of staticExport.entries) {
 			if (entry.exportName.kind === 'Name') {
-				exportNames.add(/** @type {string} */(entry.exportName.name))
-			} else if (entry.importName.kind === 'AllButDefault') {
+				if (
+					includeAll
+					|| (includeTypesOnly && entry.isType)
+					|| (includeValuesOnly && !entry.isType)
+				) {
+					/** @type {Set<string>} */
+					(exportNames)
+						.add(/** @type {string} */ (entry.exportName.name))
+				} else if (separate) {
+					/** @type {[Set<string>, Set<string>]} */
+					(exportNames)
+						[/** @type {0|1} */ (Number(entry.isType))]
+						.add(/** @type {string} */ (entry.exportName.name))
+				}
+			} else if (entry.importName.kind === 'AllButDefault' && (includeTypes || !entry.isType)) {
 				const reExportedModuleSpecifier = /** @type {ValueSpan} */(entry.moduleRequest).value
 				resolveModuleExportNames(
 					reExportedModuleSpecifier,
 					dirname(modulePath),
-					{ exportNames }
+					// @ts-expect-error
+					{ includeTypes: typesConfig, exportNames }
 				)
 			}
 		}
 	}
 
-	return asSet ? exportNames : Array.from(exportNames)
+	return asSet
+		? exportNames
+		: separate
+			? /** @type {[string[], string[]]} */ (
+				/** @type {readonly Set<string>[]} */
+				(exportNames)
+					.map(set => Array.from(set))
+			)
+			: Array.from(/** @type {Set<string>} */(exportNames))
 }
